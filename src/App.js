05d445de731300418387b31386678d2fb03041cb
lizajupa04 <lizapalominousa@gmail.com>
2024-01-16 16:39:35 -0500

    Boostrap added
    Boostrap added

--- src/App.js
+++ src/App.js
@@ -4,6 +4,13 @@ function App() {
   return (
     <div className="App">
       <h1>Hello world</h1>
+
+      <div className="container">
+        <div className="row">
+          <div className="col-6">Hello</div>
+          <div className="col-6">Hello</div>
+        </div>
+      </div>
     </div>
   );
 }
